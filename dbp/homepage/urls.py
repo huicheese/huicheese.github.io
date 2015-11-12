42b6fcd6ef2b1db4554329e5baaa13ce5ea63888
huicheese <huihui_tiang@mymail.sutd.edu.sg>
2015-11-12 13:51:01 +0700

    first commit

--- dbp/homepage/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-	url(r'^$', views.homepage,name='homepage'),
-	url(r'login',views.login,name='login'),
-	url(r'registration',views.registration,name='register'),
-]
